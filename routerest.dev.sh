1e0fff33d76a21a2877bacf517879c97de275e3f
Alexey Pechnikov <pechnikov@mobigroup.ru>
2021-05-28 13:27:59 +0700

    Drop HTTP REST output message in test script

--- routerest.dev.sh
+++ routerest.dev.sh
@@ -18,9 +18,10 @@ trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 npm start &
 
 # allow the server to start
-sleep 5
+sleep 2
 
 # test request
-curl -X POST -H 'Content-Type: application/json' -d "@../request_example1.json" "http://localhost:8080/"
+curl -X POST -H 'Content-Type: application/json' -d "@../request_example1.json" "http://localhost:8080/" > /dev/null
+#curl -X POST -H 'Content-Type: application/json' -d "@../responce_example1.json" "http://localhost:8080/"
 
 wait
